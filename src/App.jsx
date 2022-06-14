08e6cd71aefab58c01efd451bb18e8d48e6eeaf1
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-14 14:43:06 +0900

    JSXで構造を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="Type things ..." />
+        <button>Add</button>
+      </div>
+
+      <div>
+        <p>Unfinished</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>Finished</button>
+            <button>Delete</button>
+          </div>
+          <div>
+            <li>bbb</li>
+            <button>Finished</button>
+            <button>Delete</button>
+          </div>
+        </ul>
+      </div>
+
+      <div>
+        <p>Finished</p>
+        <ul>
+          <div>
+            <li>ccc</li>
+            <button>Move back</button>
+          </div>
+          <div>
+            <li>ddd</li>
+            <button>Move back</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
