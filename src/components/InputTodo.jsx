805e2778cb7171f8ad7bbcf66546dd8f2d089799
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-15 17:44:54 +0900

    7- カイゼン(CSS-in-JS)

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
+// 7- Copy style from [styles.css]
+const style = {
+  // 7- background-color => backgroundColor
+  // 7- [#e2ece9 ]=> ['#e2ece9'], [;]=>[,]
+  backgroundColor: "#e2ece9",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 // 6- Add props to export to App.jsx
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
 
   return (
-    <div className="input-area">
+    // 7- Add style={style} from [const style]
+    // 7- Delete [className="input-area"]
+    // 7- Delete [.input-area] in [styles.css]
+    <div style={style}>
       {/* 2- inputに対してStateで持っているtodoText変数,
         onChangeTodoText変数を設定 */}
       <input
